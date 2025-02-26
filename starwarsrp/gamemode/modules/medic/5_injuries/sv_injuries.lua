481004e2d30b13a9afe3b8bb1117c197a7afdd68
Deadshotlp <132559658+Deadshotlp@users.noreply.github.com>
2025-02-26 21:07:25 +0100

    Test

--- starwarsrp/gamemode/modules/medic/5_injuries/sv_injuries.lua
+++ starwarsrp/gamemode/modules/medic/5_injuries/sv_injuries.lua
@@ -36,24 +36,6 @@ PD.DM.Injury.tbl = {
 
 hook.Add("EntityTakeDamage", "DM.Injury", function(target, dmg)
 
-    --
-    --
-    --
-    --
-
-    --
-    --
-
-    --
-    --
-    --
-
-    --
-
-    --
-
-    --
-
     if target:IsPlayer() then -- and not target:LastHitGroup() == 10 then
         dmg:SetDamage(0)
         local hitGroup = 1 -- target:LastHitGroup()
