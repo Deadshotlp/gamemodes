ee26ee3aac629949786138ae211859201551fbfa
Deadshotlp <132559658+Deadshotlp@users.noreply.github.com>
2025-02-26 21:21:32 +0100

    bindblock deaktiviert

--- starwarsrp/gamemode/modules/bindblock/cl_bindblock.lua
+++ starwarsrp/gamemode/modules/bindblock/cl_bindblock.lua
@@ -2,12 +2,12 @@ PD.BB = PD.BB or {}
 
 hook.Add("OnContextMenuOpen", "PD.CheckContectMenu", function()
     -- if not LocalPlayer():IsAdmin() then return false end
-    PD.AdminCheck()
+    -- PD.AdminCheck()
 end)
 
 hook.Add("OnSpawnMenuOpen", "PD.CheckSpawnMenu", function()
     -- if not LocalPlayer():IsAdmin() then return false end
-    PD.AdminCheck()
+    -- PD.AdminCheck()
 end)
 
 function PD.AdminCheck()
