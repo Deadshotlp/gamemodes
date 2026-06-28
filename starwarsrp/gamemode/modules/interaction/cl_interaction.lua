5ab47ff9f5ce5919be200f14fb77ec613830baa1
Deadshotlp <132559658+Deadshotlp@users.noreply.github.com>
2026-06-28 22:28:42 +0200

    Test

--- starwarsrp/gamemode/modules/interaction/cl_interaction.lua
+++ starwarsrp/gamemode/modules/interaction/cl_interaction.lua
@@ -184,14 +184,13 @@ print("Entity class is: " .. ent:GetClass())
 
 
 
-
-
         if not ent:IsValid() and not PD.IA.CurrentBone.looked_at then return end
 
 
-if PD.IA.LastEntity == nil then
-    PD.IA.LastEntity = ent
+if PD.IA.LastEntity.ent == nil then
+    PD.IA.LastEntity.ent = ent
 end
+
 
 
         if ply:GetPos():Distance(ent:GetPos()) >= 100 and ply:GetPos():Distance(PD.IA.LastEntity.ent:GetPos()) >= 100 then return end
